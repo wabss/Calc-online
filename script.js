c884e6017e7664253989c0f0acdbd09ffe3911a8
wabss <jesusssa2005@gmail.com>
2025-02-06 11:16:35 -0500

    Version 1.4.0

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+//
 document.addEventListener('DOMContentLoaded', () => {
     const screen = document.getElementById('screen');
     const buttons = document.querySelectorAll('.key');
     const operators = document.querySelectorAll('.operator');
+    const erasers = document.querySelectorAll('.eraser');
     
     // Agregar eventos para los botones de números
     buttons.forEach(button => {
         button.addEventListener('click', (event) => {
             const buttonValue = event.target.textContent;
             
-            // Si el valor es el botón de resultado (=)
-            if (buttonValue === '=') {
+            screen.textContent += buttonValue;
+        });
+    });
+
+    // Agregar eventos para los botones de operadores
+    operators.forEach(operator => {
+        operator.addEventListener('click', (event) => {
+            const operatorValue = event.target.textContent;
+            if (operatorValue === '=') {
                 try {
                     // Evaluamos la expresión en la pantalla y mostramos el resultado
                     screen.textContent = eval(screen.textContent);
@@ -18,16 +27,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             } else {
                 // Agregamos el valor del botón a la pantalla
-                screen.textContent += buttonValue;
+                screen.textContent += ` ${operatorValue} `;
             }
         });
     });
+    erasers.forEach(eraser => {
+        eraser.addEventListener('click', (event) => {
+            const eraserValue = event.target.textContent;
 
-    // Agregar eventos para los botones de operadores
-    operators.forEach(operator => {
-        operator.addEventListener('click', (event) => {
-            const operatorValue = event.target.textContent;
-            screen.textContent += ` ${operatorValue} `;
-        });
-    });
+            if(eraserValue === 'AC'){
+                screen.textContent *= 0;
+            } else {
+                let newtext = screen.textContent.slice(0, -1);
+                screen.textContent = newtext;
+            }
+        })
+    })
 });
+
+console.log(window.innerHeight)
